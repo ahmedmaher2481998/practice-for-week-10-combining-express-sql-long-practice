08e5be69f39c4c91672aee044f4f669efb669397
ahmedmaher2481998 <ahmedmaher2481998@yahoo.com>
2022-10-11 19:18:06 +0200

    insert/delete/read routes implemented

--- server/routes/trees.js
+++ server/routes/trees.js
@@ -1,7 +1,56 @@
 // Instantiate router - DO NOT MODIFY
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const sqlite3 = require("sqlite3");
+const db = new sqlite3.Database(process.env.DB, sqlite3.OPEN_READWRITE);
+router.get("/", (req, res, next) => {
+	const sql = `SELECT id,tree FROM trees;`;
+	const params = [];
+	db.all(sql, params, (err, rows) => {
+		if (err) {
+			next(err);
+		}
+		// console.log(rows);
+		res.json(rows);
+	});
+});
 
+router.post("/", (req, res, next) => {
+	const { name, location, height, size } = req.body;
+	if (name && location && height && size) {
+		const sql = `INSERT INTO trees (tree,location,height_ft,ground_circumference) values (?,?,?,?);`;
+		db.all(sql, [name, location, height, size], (err) => {
+			if (err) {
+				next(err);
+			}
+			res.json({ status: "Success" });
+		});
+	} else {
+		res.send("Data Is messing ....");
+	}
+});
+router.delete("/:id", (req, res, next) => {
+	const id = req.params.id;
+	const sql = `DELETE FROM trees WHERE id=?;`;
+	db.all(sql, [id], (err) => {
+		if (err) next(err);
+		res.json({ status: "Success" });
+	});
+});
+router.get("/:id", (req, res, next) => {
+	const id = +req.params.id;
+	if (Number.isInteger(id)) {
+		const sql = `SELECT * FROM trees WHERE id=? limit 1`;
+		db.all(sql, [id], (err, rows) => {
+			if (err) next(err);
+			if (rows[0]) {
+				res.json(rows);
+			} else {
+				res.json({ status: "Error", message: "Un valid id" });
+			}
+		});
+	}
+});
 /**
  * BASIC PHASE 2, Step A - Instantiate SQLite and database
  *   - Database file: "data_source" environment variable
--- server/seed-data.sql
+++ server/seed-data.sql
@@ -1,5 +1,187 @@
 -- BASIC PHASE 1A - DROP and CREATE table
 -- Your code here
+DROP TABLE if EXISTS trees;
+
+CREATE TABLE trees(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  tree varchar(32) NOT NULL,
+  location varchar(64) NOT NULL,
+  height_ft NUMERIC(3, 1),
+  ground_circumference NUMERIC(3, 1)
+);
 
 -- BASIC PHASE 1B - INSERT seed data
 -- Your code here
+INSERT INTO
+  trees (tree, location, height_ft, ground_circumference)
+VALUES
+  (
+    "General Sherman",
+    "Sequoia National Park",
+    274.9,
+    102.6
+  ),
+  (
+    "General Grant",
+    "Kings Canyon National Park",
+    268.1,
+    107.5
+  ),
+  (
+    "President",
+    "Sequoia National Park",
+    240.9,
+    93.0
+  ),
+  ("Lincoln", "Sequoia National Park", 255.8, 98.3),
+  ("Stagg", "Private Land", 243.0, 109.0),
+  (
+    "Boole	Converse Basin	Giant ",
+    "Sequoia National Monument",
+    268.8,
+    113
+  ),
+  (
+    "Genesis	Mountain Home",
+    "Mountain Home State Forest",
+    253,
+    85.3
+  ),
+  (
+    "Franklin	Giant Forest",
+    "Sequoia National Park",
+    223.8,
+    94.8
+  ),
+  (
+    "King Arthur	Garfield",
+    "Sequoia National Park",
+    270.3,
+    104.2
+  ),
+  (
+    "Monroe	Giant Forest",
+    "Sequoia National Park",
+    247.8,
+    91.3
+  ),
+  (
+    "Robert E. Lee	Grant Grove",
+    "	Kings Canyon National Park",
+    254.7,
+    88.3
+  ),
+  (
+    "unnamed	Garfield Grove",
+    "Sequoia National Park",
+    273.1,
+    99.5
+  ),
+  (
+    "Adams	Giant Forest",
+    "Sequoia National Park",
+    250.6,
+    83.3
+  ),
+  (
+    "Ishi Giant	Kennedy Grove	Giant ",
+    "Sequoia National Monument",
+    255,
+    105.1
+  ),
+  (
+    "Column	Giant Forest",
+    "Sequoia National Park",
+    243.8,
+    93
+  ),
+  (
+    "Summit Road	Mountain Home",
+    "Mountain Home State Forest",
+    244,
+    82.2
+  ),
+  (
+    "Euclid	Mountain Home",
+    "Mountain Home State Forest",
+    272.7,
+    83.4
+  ),
+  (
+    "Washington	Mariposa Grove",
+    "Yosemite National Park",
+    236,
+    95.7
+  ),
+  (
+    "Pershing	Giant Forest",
+    "Sequoia National Park",
+    246,
+    91.2
+  ),
+  (
+    "Diamond	Atwell Grove",
+    "Sequoia National Park",
+    286,
+    95.3
+  ),
+  (
+    "Adams	Mountain Home",
+    "Mountain Home State Forest",
+    247.4,
+    94.2
+  ),
+  (
+    "Roosevelt/False Heart	Redwood",
+    "Mountain Grove	Kings Canyon National Park",
+    260,
+    80
+  ),
+  (
+    "Nelder	nelder Grove",
+    "	Sierra National Forest",
+    266.2,
+    90
+  ),
+  (
+    "AD	Atwell Grove",
+    "Sequoia National Park",
+    242.4,
+    99
+  ),
+  (
+    "Hart	Redwood Mountain Grove",
+    "Kings Canyon National Park",
+    277.9,
+    75.3
+  ),
+  (
+    "Grizzly Giant	Mariposa Grove",
+    "Yosemite National Park",
+    209,
+    92.5
+  ),
+  (
+    "Chief Sequoyah	Giant Forest",
+    "Sequoia National Park",
+    228.2,
+    90.4
+  ),
+  (
+    "Methuselah	Mountain Home",
+    "Mountain Home State Forest",
+    207.8,
+    95.8
+  ),
+  (
+    "Great Goshawk	Freeman Creek	Giant",
+    " Sequoia National Monument",
+    255.2,
+    90.2
+  ),
+  (
+    "Hamilton	Giant Forest",
+    "Sequoia National Park",
+    238.5,
+    82.6
+  );
